903d72a3c50ee1c705a0306ca751f153b8c028de
Misael Garcia <110581963+Misael2004@users.noreply.github.com>
2025-04-22 19:24:17 -0400

    Update Cards.jsx

--- books_front/books_front/src/components/Cards.jsx
+++ books_front/books_front/src/components/Cards.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import ViewImg from "./ViewImg";
-import {envs} from '../config/envs'
+
 
 export default function Cards({ books }) {
   const [view, setView] = useState("");
@@ -9,13 +9,13 @@ export default function Cards({ books }) {
   const handleView = (url) => {
     setView(url);
   };
- const imgFn = (url) => (`${envs.BASE_API/url}`)
+
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
         {books?.map((book, index) => (
           <motion.div
-            onClick={() => handleView(imgFn(book?.url))}
+            onClick={() => handleView(book?.url)}
             key={index}
             className="max-w-sm rounded-lg overflow-hidden bg-white"
             initial={{ opacity: 0, y: 50 }}
@@ -32,7 +32,7 @@ export default function Cards({ books }) {
             }}
           >
             <img
-              src={() => imgFn(book?.url)}
+              src={book?.url}
               alt={book?.title}
               className="w-full h-48 object-cover"
             />
